1e18fd0388464265ab1f2d44cb40212de59906ee
Tobias Pflug <tobias.pflug@gmail.com>
2023-10-20 23:32:35 +0200

    Use jdt language server

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 { pkgs ? import ./nix }:
 
 pkgs.mkShell {
-  buildInputs = with pkgs; [ npins jdk17 gradle java-language-server ];
+  buildInputs = with pkgs; [ npins jdk17 gradle jdt-language-server ];
 }
