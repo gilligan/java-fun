9ff25f7083f2855a57aeb15de822d3fd97e2c7af
Tobias Pflug <tobias.pflug@gmail.com>
2023-10-20 23:31:30 +0200

    Run service on port specified by HTTP_PORT
    Check if HTTP_PORT is set and otherwise default to port 8080

--- src/main/java/com/tpflug/java/DemoApplication.java
+++ src/main/java/com/tpflug/java/DemoApplication.java
@@ -1,13 +1,26 @@
 package com.tpflug.java;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
+    public static void main(String[] args) {
+      String httpPort = System.getenv("HTTP_PORT");
+      int servicePort = (httpPort != null) ? Integer.parseInt(httpPort) : 8080;
+      SpringApplication app = new SpringApplication(DemoApplication.class);
+      app.setDefaultProperties(Collections.singletonMap("server.port", servicePort));
+      app.run(args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
 }
